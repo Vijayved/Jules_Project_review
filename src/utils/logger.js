9fc7a8eac010e0eae7fc76bdde3d0b0e623c2dc8
Vijayved <162551921+Vijayved@users.noreply.github.com>
2026-03-24 17:45:57 +0530

    Delete src directory

--- src/utils/logger.js
+++
@@ -1,9 +0,0 @@
-function info(message) {
-  console.log(`[INFO] ${new Date().toISOString()} - ${message}`);
-}
-
-function error(message) {
-  console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
-}
-
-module.exports = { info, error };
